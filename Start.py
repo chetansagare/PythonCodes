a2ca29089f5555dfa47462ec6111f866e0ad1688
chetansagare <chetansagare1@gmail.com>
2018-04-25 00:30:44 +0530

    first commit

---
+++ Start.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+
+#print("Hello World")
+import numpy as np
+height=[12.4,35.6,45.7,67.8,87.2]
+np_height=np.array(height)
+print(np_height**2)
+print("Hie Chetan")
+print(np.array([True, 1, 2]) + np.array([3, 4, False]))
---
+++ test.py
@@ -0,0 +1,6 @@
+
+if True or False and True:
+    print("Hello")
+else:
+    print("Bye")
+
